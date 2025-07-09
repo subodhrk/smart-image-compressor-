ecd62d6653467b9e9d0040f25e60cd81565904e0
Subodh Kamble <160238668+subodhrk@users.noreply.github.com>
2025-07-09 10:53:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def editor():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render will set PORT
+    app.run(host='0.0.0.0', port=port)
 
